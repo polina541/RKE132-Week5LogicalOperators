2cb8549c550b805f4fb3e3a5e23f9d3e0dbbbca9
Polina Smm. <polina.somova@tktk.ee>
2024-02-26 13:01:47 +0200

    add Week5 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int math, biology, chemistry;
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter you biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90  && biology >= 90  && chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
+
+
---
+++ OROperator/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter you biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if ((math >= 90 && chemistry >= 90 ) || (math >= 90 && biology >= 90)  || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You are accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
